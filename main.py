adf4e118bbf2100fbf2ec97049f98cbde5e37ab4
abinaya2-2 <143793696+abinaya2-2@users.noreply.github.com>
2023-09-21 22:20:54 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-year=int(input("Enter year to be checked:"))
-if(year%4==0 and year%100!=0 or year%400==0):
-  print("The year is a leap year!") 
-else:
-  print("The year isn't a leap year!")
+def linear_search_product(products, target):
+    indices = []
+    
+    for i in range(len(products)):
+        if products[i] == target:
+            indices.append(i)
+    
+    return indices
+products = ["apple", "banana", "apple", "grapes", "apple"]
+target = "apple"
+print(linear_search_product(products, target))
+
+#Output:
+#[0, 2, 4]
+
+
